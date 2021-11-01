7224bdd7335f799e5e00de3ecbf9c4730c45ef83
MatD1 <64875762+MatD1@users.noreply.github.com>
2021-11-02 10:13:19 +1100

    Centering Categorys page.

--- pages/categorys.js
+++ pages/categorys.js
@@ -1,4 +1,4 @@
-import { Alert, AlertIcon, AlertDescription, Heading, Button, Box, Grid, Icon, VStack } from '@chakra-ui/react';
+import { Alert, AlertIcon, AlertDescription, Heading, Button, Box, Grid, Icon, VStack, Center } from '@chakra-ui/react';
 import axios from 'axios';
 import Link from 'next/link'
 import {BsDoorClosed} from 'react-icons/bs'
@@ -23,10 +23,12 @@ const Categorys = ({ papers, error }) => {
         <Grid templateRows="repeat(5, 1fr)" gap={6} pl={4} pb={10} pr={4}>
           {papers.map(paper => ( 
           <Box key={paper.id} w="100%" h="10" mb={10}>
+            <Center>
             <Heading>{paper.name}</Heading>
             <Link href={`/${paper.web_link}`}>
               <Button rightIcon={<Icon as={BsDoorClosed} />} colorScheme="orange">See Papers</Button>
             </Link>
+            </Center>
           </Box>
           ))}
         </Grid>
